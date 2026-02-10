9e97f1f7ca2d7f1768c6c9c04714d93e243ea395
zjxhn <johnlg0112@gmail.com>
2026-02-10 00:03:41 -0500

    añadi marketplace y offers

---
+++ app/marketplace/page.tsx
@@ -0,0 +1,8 @@
+export default function MarketplacePage() {
+  return(
+    <main className="p-6">
+      <h1 className="text-2xl font-semibold">Marketplace</h1>
+      <h1 className="mt-2 text-sm text-gray-600">Aquí irá el listado de ofertas</h1>
+    </main>
+  );
+}
